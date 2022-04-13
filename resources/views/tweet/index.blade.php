b5d27d21652aa9e6826aea61a5b53def38f25f6d
devWillS <>
2022-04-14 00:05:48 +0900

    ルーティングサンプル

---
+++ resources/views/tweet/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" 
+            content="width=device-width, user-scalable=no, initial-scale=1.0,
+            maxium-scale=1.0, minium-scale=1.0">
+        <title>つぶやきアプリ</title>
+    </head>
+    <body>
+        <h1>つぶやきアプリ</h1>
+        <p>{{ $name }}</p>
+    </body>
+</html>
