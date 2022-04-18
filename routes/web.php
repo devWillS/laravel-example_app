7fac1948837443854f3823ebe72bff94574c5e3b
devWillS <>
2022-04-18 14:27:31 +0900

    ログイン機能の追加

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\Sample\IndexController;
-use App\Http\Controllers\Tweet\CreateController;
-use App\Http\Controllers\Tweet\DeleteController;
-use App\Http\Controllers\Tweet\IndexController as TweetIndexController;
-use App\Http\Controllers\Tweet\Update\IndexController as UpdateIndexController;
-use App\Http\Controllers\Tweet\Update\PutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,36 +27,14 @@ Route::get(
     [IndexController::class, 'showId']
 );
 
-Route::get(
-    '/tweet',
-    TweetIndexController::class
-)
-    ->name('tweet.index');
-
-Route::post(
-    '/tweet/create',
-    CreateController::class
-)
-    ->name('tweet.create');
-
-Route::get(
-    '/tweet/update/{tweetId}',
-    UpdateIndexController::class
-)
-    ->name('tweet.update.index')
-    ->where('tweetId', '[0-9]+');
-
-Route::put(
-    '/tweet/update/{tweetId}',
-    PutController::class
-)
-    ->name('tweet.update.put')
-    ->where('tweetId', '[0-9]+');
-Route::delete(
-    '/tweet/delete/{tweetId}',
-    DeleteController::class
-)
-    ->name('tweet.delete');
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+Route::middleware('auth')->group(function () {
+    Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+        ->name('tweet.create'); //->middleware('auth')は削除
+    Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
+    Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
+    Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
